c01c7b8abd8ffa0acb71a558c22988506687b1ad
Nikos <nikospampg4@gmail.com>
2026-05-12 15:17:38 +0300

    Add theming and theme switcher button.

--- app/src/main/java/com/pampoukidis/streamcoretv/MainActivity.kt
+++ app/src/main/java/com/pampoukidis/streamcoretv/MainActivity.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.pampoukidis.streamcoretv.ui.theme.StreamCoreTVTheme
@@ -18,15 +25,51 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            StreamCoreTVTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            var darkTheme by rememberSaveable { mutableStateOf(false) }
+
+            StreamCoreTVTheme(darkTheme = darkTheme) {
+                MainScreen(
+                    darkTheme = darkTheme,
+                    onToggleTheme = { darkTheme = !darkTheme },
+                )
             }
         }
+    }
+}
+
+@Composable
+fun MainScreen(
+    darkTheme: Boolean,
+    onToggleTheme: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Scaffold(modifier = modifier.fillMaxSize()) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+        ) {
+            ThemeToggleButton(
+                darkTheme = darkTheme,
+                onClick = onToggleTheme,
+                modifier = Modifier.align(Alignment.TopEnd),
+            )
+            Greeting(name = "Android")
+        }
+    }
+}
+
+@Composable
+fun ThemeToggleButton(
+    darkTheme: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+    ) {
+        Text(text = if (darkTheme) "Light" else "Dark")
     }
 }
 
@@ -41,7 +84,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    StreamCoreTVTheme {
-        Greeting("Android")
+    val isDarkTheme = false
+    StreamCoreTVTheme(darkTheme = isDarkTheme) {
+        MainScreen(
+            darkTheme = isDarkTheme,
+            onToggleTheme = {},
+        )
     }
 }
